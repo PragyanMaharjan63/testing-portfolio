2f099c97a81c80de45a42ea628ccf5ed4d305222
Pragyan Maharjan <pragyanmaharjan6k@gmail.com>
2025-03-08 20:33:30 +0545

    added animation to the list

---
+++ animation.js
@@ -0,0 +1,45 @@
+let project = document.getElementById('project')
+let projectTitle = document.getElementById("projects-description");
+const container=document.getElementById('container');
+const sideArrow=document.getElementById('sideArrow');
+const projectLine=document.getElementById('projectLine');
+let display = () =>{
+//    console.log('clicked');
+//    console.log(projectTitle.classList);
+    if(projectTitle.classList.contains("opacity-0")){
+       
+        project.classList.add("text-xl");
+        project.classList.add("font-bold");
+        projectTitle.classList.remove("opacity-0");
+        projectTitle.classList.remove("max-h-0");
+        projectTitle.classList.add("m-5");
+        projectTitle.classList.add("mt-0");
+        projectTitle.classList.add("opacity-100");
+        // sideArrow.classList.remove("-rotate-90");
+        sideArrow.classList.add("-rotate-90");
+        projectTitle.classList.add("max-h-screen");
+        projectLine.classList.remove("h-[0px]");
+        projectLine.classList.add("h-[100px]");
+        
+    }
+    else{
+        // project.classList.remove("text-xl");
+        project.classList.remove("font-bold");
+        projectTitle.classList.remove("opacity-100");
+        projectTitle.classList.remove("max-h-screen");
+        projectTitle.classList.add("max-h-0");
+        projectTitle.classList.add("opacity-0");
+        projectTitle.classList.remove("m-5");
+        projectTitle.classList.remove("mt-0");
+        sideArrow.classList.remove("-rotate-90");
+        projectLine.classList.remove("h-[100px]");
+        projectLine.classList.add("h-[0px]");
+    }
+    // projectTitle.classList.remove("hidden");
+
+}
+
+project.addEventListener('click', display)
+// project.addEventListener('click', container.classList.toggle('max-w-[100%]'))
+
+ 
--- script.js
+++ script.js
@@ -1,42 +1,12 @@
-let project = document.getElementById('project')
-let projectTitle = document.getElementById("projects-description");
-const container=document.getElementById('container');
-const sideArrow=document.getElementById('sideArrow');
-const projectLine=document.getElementById('projectLine');
-let display = () =>{
-   console.log('clicked');
-   console.log(projectTitle.classList);
-    if(projectTitle.classList.contains("opacity-0")){
-       
-        project.classList.add("text-xl");
-        project.classList.add("font-bold");
-        projectTitle.classList.remove("opacity-0");
-        projectTitle.classList.remove("max-h-0");
-        projectTitle.classList.add("m-5");
-        projectTitle.classList.add("mt-0");
-        projectTitle.classList.add("opacity-100");
-        // sideArrow.classList.remove("-rotate-90");
-        sideArrow.classList.add("-rotate-90");
-        projectTitle.classList.add("max-h-screen");
-        projectLine.classList.remove("h-[0px]");
-        projectLine.classList.add("h-[100px]");
-        
+const getProjects = async () =>{
+    try{
+        const data = await fetch("http://127.0.0.1:5500/projects/projects.json")
+        let project = await data.json()
+        console.log(project)
     }
-    else{
-        // project.classList.remove("text-xl");
-        project.classList.remove("font-bold");
-        projectTitle.classList.remove("opacity-100");
-        projectTitle.classList.remove("max-h-screen");
-        projectTitle.classList.add("max-h-0");
-        projectTitle.classList.add("opacity-0");
-        projectTitle.classList.remove("m-5");
-        projectTitle.classList.remove("mt-0");
-        sideArrow.classList.remove("-rotate-90");
-        projectLine.classList.remove("h-100px");
-        projectLine.classList.add("h-[0px]");
+    catch(err){
+        console.log(err)
     }
-    // projectTitle.classList.remove("hidden");
 }
 
-project.addEventListener('click', display)
-project.addEventListener('click', container.classList.toggle('max-w-[100%]'))
+getProjects()
